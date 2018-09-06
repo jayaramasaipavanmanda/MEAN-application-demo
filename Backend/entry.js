a16cc1d2b90e9566ad6259e3e01cd1f5a1b65fe7
jayaramasaipavanmanda <saipavan.m12@gmail.com>
2018-09-06 08:12:47 +0530

    first commit for chwcking squash

--- Backend/entry.js
+++ Backend/entry.js
@@ -1,3 +1,6 @@
+//killall command --taskkill/f /im node.exe
+
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
@@ -5,10 +8,38 @@ var cors = require('cors');
 var app = express();
 var path = require('path');
 const route = require('./routes/routes');
-mongoose.connect('mongodb://localhost:27017/shoppinglist');
+// mongoose.connect('mongodb://localhost:27017/shoppinglist');
 
-mongoose.connection.on('connected', () => { console.log("connected to port 27017") });
-mongoose.connection.on('error', (err) => { console.log(err) });
+// mongoose.connection.on('connected', () => { console.log("connected to port 27017") });
+// mongoose.connection.on('error', (err) => { console.log(err) });
+const cluster = require('cluster');
+const http = require('http');
+const numCPUs = require('os').cpus().length;
+function masterProcess() {
+    console.log(`Master ${process.pid} is running`);
+
+    for (let i = 0; i < numCPUs; i++) {
+        console.log(`Forking process number ${i}...`);
+        cluster.fork();
+    }
+
+    process.exit();
+}
+
+function childProcess() {
+    console.log(`Worker ${process.pid} started`);
+
+    process.on('message', function (message) {
+        console.log(`Worker ${process.pid} recevies message '${JSON.stringify(message)}'`);
+    });
+
+    console.log(`Worker ${process.pid} sends message to master...`);
+    process.send({ msg: `Message from worker ${process.pid}` });
+
+    console.log(`Worker ${process.pid} finished`);
+}
+
+childProcess();
 
 function multiply(num) {
     return new Promise((resolve, reject) => {
@@ -34,7 +65,8 @@ multiply("hello").catch(err => { throw new Error('Error occured') })
     .catch((err) => console.log(err.error));
 
 
-const PORT = 3000;
+
+const PORT = 4200;
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/api', route);
@@ -44,5 +76,6 @@ app.use('/api', route);
 // })
 
 
-
+app.on('connect',()=>{ console.log('connected')});
 app.listen(PORT, () => { console.log("running on port:" + PORT) });
+
--- Backend/routes/routes.js
+++ Backend/routes/routes.js
@@ -6,7 +6,7 @@ module.exports = router;
 const item = require('../model/shoppingItem');
 
 
-router.get('/items', (req, res, next) => {
+router.get('/items', (req, res) => {
     item.find((err, items) => {
         if (err) {
             res.json(err);
@@ -17,7 +17,7 @@ router.get('/items', (req, res, next) => {
     })
 
 });
-router.post('/item', (req, res, next) => {
+router.post('/item', (req, res) => {
     let newItem = new item({
         itemName: req.body.itemName,
         numberOfItems: req.body.numberOfItems,
@@ -29,20 +29,20 @@ router.post('/item', (req, res, next) => {
             (res.json(err));
         }
         if (item) {
-            res.json({ msg: "added successfully" });
+            res.json({msg: "added successfully"});
         }
 
     })
 });
 router.put('/item/:id', (req, res, next) => {
-    item.findByIdAndUpdate({ _id: req.params.id }, {
-        $set: {
-            itemName: req.body.itemName,
-            numberOfItems: req.body.numberOfItems,
-            bought: req.body.bought
+    item.findByIdAndUpdate({_id: req.params.id}, {
+            $set: {
+                itemName: req.body.itemName,
+                numberOfItems: req.body.numberOfItems,
+                bought: req.body.bought
 
-        }
-    },
+            }
+        },
         function (err, result) {
             if (err) {
                 (res.json(err));
@@ -56,13 +56,13 @@ router.put('/item/:id', (req, res, next) => {
 
 
 });
-router.delete('/item/:id', (req, res, next) => {
-    item.remove({ _id: req.params.id }, function (err, result) {
+router.delete('/item/:id', (req, res) => {
+    item.remove({_id: req.params.id}, function (err) {
         if (err) {
             res.json(err);
         }
         else {
-            res.json({ msg: "deleted successfully" });
+            res.json({msg: "deleted successfully"});
         }
     })
 
